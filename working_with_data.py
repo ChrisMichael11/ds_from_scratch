ece6f5d9ec0d9797e2db67d03f1af36a81c6f1af
Chris Michael <christopher.m.michael@gmail.com>
2016-09-23 06:37:46 -0700

    Updating working_with_data.py

--- working_with_data.py
+++ working_with_data.py
@@ -103,7 +103,112 @@ def make_scatterplot_matrix():
 
     for i in range(num_columns):
         for j in range(num_columns):
+            #  Scatter column_j on the x-axis vs column_i on the y-axis
+            if i != j: ax[i][j].scatter(get_coulmn(data, j), get_column(data, i))
 
+            #  Unless i == j, then show series
+        else: ax[i][j].annotate("Series " + str(i), (0.5, 0.5),
+                                xycoords='Axes Fraction',
+                                ha="center", va="center")
+
+        #  Hide axis labels, except at left and bottom charts
+        if i < num_columns - 1: ax[i][j].xaxis.set_visible(False)
+        if j > 0: ax[i][j].yaxis.set_visible(False)
+
+    #  Fix bottom right and top left axis lables, which are wrong
+    #  Chars only ahve text!
+    ax[-1][-1].set_xlim(ax[0][-1].get_xlim())
+    ax[0][0].set_ylim(ax[0][1].gt_ylim())
+
+    plt.show()
+
+def parse_row(input_row, parsers):
+    """
+    Given list of parsers (can be None), apply appropriate one to each element
+    of input_row
+    """
+    return [parser(value) if parser is not None else value for value, parser
+                    in zip(input_row, parser)]
+
+def parse_rows_with(reader, parsers):
+    """
+    Wrap a reader to apply parser to each of its rows
+    """
+    for row in reader:
+        yield parse_row(row, parsers)
+
+def try_or_none(f):
+    """
+    Wraps f to return None if f raises an exception.  Assumes f takes one input_dict
+    """
+    def f_or_none(x):
+        try: return f(x)
+        except: return None
+    return f_or_none
+
+
+def try_parse_field(field_name, value, parser_dict):
+    """
+    Try to parse value using function from parser_dict
+    """
+    parser = parser_dict.get(field_name)            #  Returns None, if no entry
+    if parser is not None:
+        return try_or_none(parser)(value)
+    else:
+        return value
+
+
+##  MANIPULATING DATA
+
+def picker(field_name):
+    """
+    returns function that picks a field out of a dict
+    """
+    return lambda row: row[field_name]
+
+
+def pluck(field_name, rows):
+    """
+    Turn list of dicts into list of field_name values
+    """
+    return map(picker(field_name), rows)
+
+
+def group_by(grouper, rows, value_transform=None):
+    """
+    Group rows by result, can apply values xform too
+    """
+    grouped = defaultdict(list)
+    for row in rows:
+        grouped[grouper(row)].append(row)
+    if value_transform is None:
+        return grouped
+    else:
+        return {key: value_transform(rows) for key, rows in grouped.iteritems()}
+
+
+def percent_price_change(yesterday, today):
+    """
+    Calculate % price change from yesterday based on closing data
+    """
+    return today["closing_price"] / yesterday["closing_price"] - 1
+
+
+def day_over_day_changes(grouped_rows):
+    """
+    Calculate percent price change for multiple groups
+    """
+    #  Sort rows by date
+    ordered = sorted(grouped_rows, key=picker("date"))
+
+    #  ZIP with an offset toget pairs of consecutive days
+    return [{"symbol" : today["symbol"],
+             "date" : today["date"],
+             "change" : percent_price_change(yesterday, today)}
+             for yesterday, today in zip(ordered, ordered[1:])]
+
+
+##  RESCALING DATA
 
 
 if __name__ == "__main__":
